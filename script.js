001c67d674f993bd62e9711adf167403b3cad2c1
Joana1007 <115550510+Joana1007@users.noreply.github.com>
2022-12-11 13:44:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let nome = "Joana";
+console.log(nome)
+let age = "20";
+console.log(age)
+
+
+console.log(age*7)
+
+
+let students = ["Gonçalo", "Matias", "David"];
+console.log(students[0]);
+
+
+  let person = {
+    nome: "Joana",
+    age: 20,
+    isStudent: true
+  };
+  console.log(person.nome);
+
+  
+  let friends = [
+    {
+      nome: "Gonçalo",
+      age: 20,
+      isStudent: true
+    },
+    {
+      name: "Matias",
+      age: 18,
+      isStudent: true
+    },
+    {
+      name: "David",
+      age: 18,
+      isStudent: true
+    }
+  ];
+  console.log(friends[0].nome);
